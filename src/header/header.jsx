c15845b9030d0400ec614fb750412025758f9dae
Bahadir UNGOR <bahadirungor@gmail.com>
2023-09-17 18:47:16 +0300

    add project image

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,12 +1,13 @@
 import Switch from "../darkTheme/switch"
 import './header.css'
+import logo from '../assets/images/favicon.ico'
 
 export default function Header(){
     
     return(
         <header>
             <div className="header">Pokémon Memory Game</div>
-            <img src="/favicon.ico" alt="logo"/>
+            <img src={logo} alt="logo"/>
             <Switch/>
         </header>
     )
